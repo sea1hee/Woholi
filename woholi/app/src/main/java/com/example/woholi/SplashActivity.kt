5ff1967cc6576afe44058c95cc42edef60b7d362
sea1hee <sea11hee@gmail.com>
2021-07-12 18:59:58 +0900

    [ADD] 스플래쉬

---
+++ woholi/app/src/main/java/com/example/woholi/SplashActivity.kt
@@ -0,0 +1,17 @@
+package com.example.woholi
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class SplashActivity: AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val intent = Intent(this, LoginActivity::class.java)
+        startActivity(intent)
+        finish()
+    }
+
+}
