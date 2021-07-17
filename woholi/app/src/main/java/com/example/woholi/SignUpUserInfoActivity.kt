f6ded2c5601ef47fe3fce22408c3cf2b2a6fa644
sea1hee <sea11hee@gmail.com>
2021-07-17 21:26:21 +0900

    [ADD] 정보 입력 페이지 버튼 추가

--- woholi/app/src/main/java/com/example/woholi/SignUpUserInfoActivity.kt
+++ woholi/app/src/main/java/com/example/woholi/SignUpUserInfoActivity.kt
@@ -1,11 +1,46 @@
 package com.example.woholi
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.woholi.databinding.ActivitySignUpUserInfoBinding
+import com.google.firebase.auth.FirebaseAuth
 
 class SignUpUserInfoActivity : AppCompatActivity() {
+
+    val binding by lazy {ActivitySignUpUserInfoBinding.inflate(layoutInflater)}
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_sign_up_user_info)
+        setContentView(binding.root)
+
+        // DB 조회해서 이미 있으면 Main으로 넘기기
+
+
+        binding.button.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+
+
     }
+
+    private fun getUserInfo(){
+        val user = FirebaseAuth.getInstance().currentUser
+        user?.let {
+            // Name, email address, and profile photo Url
+            val name = user.displayName
+            val email = user.email
+            val photoUrl = user.photoUrl
+
+            // Check if user's email is verified
+            val emailVerified = user.isEmailVerified
+
+            // The user's ID, unique to the Firebase project. Do NOT use this value to
+            // authenticate with your backend server, if you have one. Use
+            // FirebaseUser.getToken() instead.
+            val uid = user.uid
+        }
+    }
+
 }
