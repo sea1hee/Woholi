68365a815e3d1d871e5ab6180649749ad348a506
sea1hee <sea11hee@gmail.com>
2021-07-17 15:15:31 +0900

    [ADD] 네비게이션바 추가

--- woholi/app/src/main/java/com/example/woholi/MainActivity.kt
+++ woholi/app/src/main/java/com/example/woholi/MainActivity.kt
@@ -2,10 +2,54 @@ package com.example.woholi
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.woholi.Navigation.*
+import com.example.woholi.databinding.ActivityLoginBinding
+import com.example.woholi.databinding.ActivityMainBinding
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(){
+
+    val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
+
+    val homeFragemnt by lazy { HomeFragment()}
+    val checkListFragment by lazy { CheckListFragment() }
+    val diaryFragment by lazy{ DiaryFragment() }
+    val vsFragment by lazy { VsFragment() }
+    val userInfoFragment by lazy { UserInfoFragment() }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+
+        supportFragmentManager.beginTransaction().replace(R.id.frameLayout, homeFragemnt).commit()
+
+        binding.navigation.setOnItemSelectedListener(onNavigationItemSelectedListener)
     }
+
+    private val onNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener {
+        when(it.itemId) {
+            R.id.nvg_home -> {
+                supportFragmentManager.beginTransaction().replace(R.id.frameLayout, homeFragemnt)
+                    .commit()
+            }
+            R.id.nvg_checkList -> {
+                supportFragmentManager.beginTransaction()
+                    .replace(R.id.frameLayout, checkListFragment).commit()
+            }
+            R.id.nvg_diary -> {
+                supportFragmentManager.beginTransaction().replace(R.id.frameLayout, diaryFragment)
+                    .commit()
+            }
+            R.id.nvg_vs -> {
+                supportFragmentManager.beginTransaction().replace(R.id.frameLayout, vsFragment)
+                    .commit()
+            }
+            else -> {
+                supportFragmentManager.beginTransaction()
+                    .replace(R.id.frameLayout, userInfoFragment).commit()
+            }
+        }
+        true
+    }
+
 }
