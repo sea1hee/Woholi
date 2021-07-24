55458126cda2d91a917f04a90b2372504eb81905
sea1hee <sea11hee@gmail.com>
2021-07-24 16:18:25 +0900

    [add] 쇼핑리스트 플로팅버튼 구현 새로운 카테고리추가 기능 구현

--- woholi/app/src/main/java/com/example/woholi/Navigation/CheckListFragment.kt
+++ woholi/app/src/main/java/com/example/woholi/Navigation/CheckListFragment.kt
@@ -1,16 +1,24 @@
 package com.example.woholi.Navigation
 
+import android.app.AlertDialog
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.DatePicker
+import android.widget.EditText
 import com.example.woholi.MainActivity
+import com.example.woholi.Model.CurrentUser
 import com.example.woholi.Navigation.checklist.shopping.ShoppingListFragment
 import com.example.woholi.Navigation.checklist.TravelItemFragment
 import com.example.woholi.Navigation.checklist.shopping.NewShoppingFragment
 import com.example.woholi.R
 import com.example.woholi.databinding.FragmentCheckListBinding
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
+import java.text.SimpleDateFormat
+import java.util.*
 
 class CheckListFragment : Fragment() {
 
@@ -40,8 +48,26 @@ class CheckListFragment : Fragment() {
         }
 
         binding!!.fabAdd.setOnClickListener {
-            (activity as MainActivity).setFlag(5)
+            //(activity as MainActivity).setFlag(5)
+            val builder = AlertDialog.Builder(requireContext())
+            val dialogView = layoutInflater.inflate(R.layout.dialog_shopping, null)
+            val data = HashMap<String, Any>()
+            builder.setView(dialogView)
+                    .setPositiveButton("확인") { dialogInterface, i ->
+                        val newCategory = dialogView.findViewById<EditText>(R.id.category).text.toString()
+                        Firebase.firestore.collection("users").document(CurrentUser.uid)
+                                .collection("checklist").document("shoppinglist").collection("first")
+                                .document(newCategory).set(data)
 
+                        Firebase.firestore.collection("users").document(CurrentUser.uid)
+                                .collection("checklist").document("shoppinglist").collection("first")
+                                .document(newCategory).collection("second")
+                                .document("newCategory!").set(hashMapOf("isChecked" to true))
+
+                        childFragmentManager.beginTransaction().replace(R.id.frameLayout_checklist, ShoppingListFragment()).commit()
+                        binding!!.fabAdd.visibility = View.VISIBLE
+                    }
+                    .show()
         }
         return binding!!.root
     }
