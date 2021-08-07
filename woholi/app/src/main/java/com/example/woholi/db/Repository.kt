961d9590a93ddb4bdedc3299b3c9b0cff2bcb7f0
sea1hee <sea11hee@gmail.com>
2021-08-07 18:14:13 +0900

    refactor: diary veiwmodel 적용

---
+++ woholi/app/src/main/java/com/example/woholi/db/Repository.kt
@@ -0,0 +1,45 @@
+package com.example.woholi.db
+
+import android.util.Log
+import com.example.woholi.model.CurrentUser
+import com.example.woholi.model.Diary
+import com.google.android.gms.tasks.Task
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.QuerySnapshot
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.firestore.ktx.toObject
+import com.google.firebase.ktx.Firebase
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.async
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.tasks.await
+
+class Repository {
+
+    val TAG = "Repository"
+    val db = Firebase.firestore
+
+    suspend fun getDiary(): List<Diary>{
+        val cour = CoroutineScope(Dispatchers.IO).async {
+            val deferred = readDiary().await().documents
+            val diaryList :MutableList<Diary> = mutableListOf()
+            for (document in deferred) {
+                if (document != null) {
+                    val newDiary: Diary? = document.toObject(Diary::class.java)
+                    if (newDiary != null) {
+                        diaryList.add(newDiary)
+                        Log.d(TAG, "${newDiary.url}")
+                    }
+                }
+            }
+            diaryList
+        }
+        return cour.await()
+    }
+
+    suspend fun readDiary(): Task<QuerySnapshot> {
+        return db.collection("users").document(CurrentUser.uid)
+                .collection("diary").get()
+    }
+}
